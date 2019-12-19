9464d47b88df52cf8d637934f0ca3ffb9c79a308
Valeh Salimov <valehsalimov.vs@gmail.com>
2019-12-19 14:10:12 +1100

    добавили спиннер. разделили логику

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-8zpkyg79ac2">
+      <div className="ldio-gzp0v0efo8d">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
