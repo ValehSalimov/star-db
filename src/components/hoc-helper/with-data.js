b23d5e4933bbabef7ed2710a550522cef54ac3f0
Valeh Salimov <valehsalimov.vs@gmail.com>
2019-12-25 19:19:08 +1100

    Рефакторинг

--- src/components/hoc-helper/with-data.js
+++ src/components/hoc-helper/with-data.js
@@ -6,6 +6,8 @@ const withData = View => {
   return class extends Component {
     state = {
       data: null,
+      loading: true,
+      error: false,
     };
 
     componentDidUpdate(prevProps) {
@@ -13,23 +15,41 @@ const withData = View => {
         this.update();
       }
     }
-    
+
     componentDidMount() {
       this.update();
     }
-    
+
     update() {
-      this.props.getData().then(data => {
-        this.setState({
-          data,
-        });
+      this.setState({
+        loading: true,
+        error: false,
       });
+
+      this.props
+        .getData()
+        .then(data => {
+          this.setState({
+            data,
+            loading: false,
+          });
+        })
+        .catch(() => {
+          this.setState({
+            error: true,
+            loading: false,
+          });
+        });
     }
     render() {
-      const { data } = this.state;
+      const { data, loading, error } = this.state;
 
-      if (!data) {
+      if (!data || loading) {
         return <Spinner />;
+      }
+
+      if (error) {
+        return <ErrorIndicator />;
       }
 
       return <View {...this.props} data={data} />;
